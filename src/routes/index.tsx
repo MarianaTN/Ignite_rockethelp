4e9912047d47df390e10210827cf5d195a20d9cc
Mariana Trevizani <mariana.trevizani@yahoo.com.br>
2022-11-08 00:22:36 -0300

    codigos desenvolvidos durante as aulas

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { useState, useEffect } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
+
+import { SignIn } from '../screens/SignIn'
+import { AppRoutes } from './app.routes'
+import { Loading } from '../components/Loading';
+
+export function Routes(){
+    const [loading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+    
+    useEffect(() => {
+        const subscriber = auth()
+        .onAuthStateChanged(response => {
+            setUser(response);
+            setIsLoading(false);
+        });
+
+        return subscriber;
+    },[]);
+    
+    if(loading){
+        return <Loading />
+    }
+
+    return(
+        <NavigationContainer>
+            {user ? <AppRoutes /> : <SignIn />}
+        </NavigationContainer>
+    )
+}
